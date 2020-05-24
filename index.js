4ef66f09fb3815ff919c5759f67f5cff70402995
Hameed Al Naser <alnaser@sihatkom.com>
2020-05-24 13:58:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,11 +17,4 @@ function moveDodgerLeft(){
   if (left > 0) {
   dodger.style.left = `${left - 1}px`;
 }
-}
-function moveDodgerRight(){
-  let leftNumbers = dodger.style.left.replace("px", "");
-  let left = parseInt(leftNumbers, 10);
-  if (left > 0) {
-  dodger.style.left = `${left + 1}px`;
-}
-}
+
